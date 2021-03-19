5bc4c7f4569e45bcd7045d7e2081303df46e043b
Bulat <programming.languages.only@gmail.com>
2021-03-19 15:37:36 +0300

    #12 Creating service - ConferenceService.java

---
+++ BulConferences/src/main/java/ru/itis/conferences/services/ConferenceService.java
@@ -0,0 +1,18 @@
+package ru.itis.conferences.services;
+
+import org.springframework.stereotype.Service;
+import ru.itis.conferences.models.Conference;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface ConferenceService {
+    List<Conference> findAll();
+
+    Optional<Conference> find(String name);
+
+    void delete(Conference entity);
+
+    void add(Conference entity);
+}
