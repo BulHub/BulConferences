6b9c20ee00146072f1a23d0e57bd325eb3f107c5
Bulat <programming.languages.only@gmail.com>
2021-03-19 13:31:55 +0300

    #5 Changing entity - User.java

--- BulConferences/src/main/java/ru/itis/conferences/models/User.java
+++ BulConferences/src/main/java/ru/itis/conferences/models/User.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -20,7 +21,7 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements Comparable<User>{
 
     /**
      * User display name
@@ -64,6 +65,12 @@ public class User extends BaseEntity {
             inverseJoinColumns = {@JoinColumn(name = "feedback_id", referencedColumnName = "id")})
     private List<Feedback> feedbacks;
 
+    @ManyToMany(mappedBy = "users", fetch = FetchType.LAZY)
+    private Set<Conference> conferences;
+
+    @ManyToMany(mappedBy = "users", fetch = FetchType.LAZY)
+    private List<Report> reports;
+
     /**
      * Constructor for testing spring security
      *
@@ -73,5 +80,48 @@ public class User extends BaseEntity {
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     *
+     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+     * <tt>y.compareTo(x)</tt> throws an exception.)
+     *
+     * <p>The implementor must also ensure that the relation is transitive:
+     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+     * <tt>x.compareTo(z)&gt;0</tt>.
+     *
+     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+     * all <tt>z</tt>.
+     *
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+     * class that implements the <tt>Comparable</tt> interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     *
+     * <p>In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(User o) {
+        return email.compareTo(o.getEmail());
     }
 }
