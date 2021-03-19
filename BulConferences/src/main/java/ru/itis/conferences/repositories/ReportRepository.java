81b57c0466201a634f3e09cab6368c2b6a8aad9a
Bulat <programming.languages.only@gmail.com>
2021-03-19 19:08:50 +0300

    #23 Adding repository - ReportRepository.java

---
+++ BulConferences/src/main/java/ru/itis/conferences/repositories/ReportRepository.java
@@ -0,0 +1,15 @@
+package ru.itis.conferences.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.itis.conferences.models.Audience;
+import ru.itis.conferences.models.Report;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    Optional<Report> findByName(String name);
+    List<Report> findAllByAudience(Audience audience);
+}
