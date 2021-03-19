acc3038101322e810f5be2dc4b526863d0e0150f
Bulat <programming.languages.only@gmail.com>
2021-03-19 12:00:16 +0300

    #3 Adding entity - Conference.java

---
+++ BulConferences/src/main/java/ru/itis/conferences/models/Conference.java
@@ -0,0 +1,27 @@
+package ru.itis.conferences.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "conference")
+public class Conference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description", length = 500)
+    private String description;
+}
