1538ad3fe0b2d19207fbd3286cb85a81a71b2e45
Bulat <programming.languages.only@gmail.com>
2021-03-20 17:57:52 +0300

    #48 Updating service - ConferenceServiceImpl.java

--- BulConferences/src/main/java/ru/itis/conferences/services/impl/ConferenceServiceImpl.java
+++ BulConferences/src/main/java/ru/itis/conferences/services/impl/ConferenceServiceImpl.java
@@ -2,21 +2,31 @@ package ru.itis.conferences.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.itis.conferences.dto.ConferenceDto;
+import ru.itis.conferences.utils.UserSecurity;
 import ru.itis.conferences.models.Conference;
+import ru.itis.conferences.models.User;
 import ru.itis.conferences.repositories.ConferenceRepository;
 import ru.itis.conferences.services.ConferenceService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ConferenceServiceImpl implements ConferenceService {
 
     private final ConferenceRepository conferenceRepository;
+    private final ru.itis.conferences.services.UserService userService;
+    private final UserSecurity userSecurity;
 
     @Autowired
-    public ConferenceServiceImpl(ConferenceRepository conferenceRepository) {
+    public ConferenceServiceImpl(ConferenceRepository conferenceRepository, ru.itis.conferences.services.UserService userService, UserSecurity userSecurity) {
+        this.userService = userService;
         this.conferenceRepository = conferenceRepository;
+        this.userSecurity = userSecurity;
     }
 
     @Override
@@ -37,5 +47,40 @@ public class ConferenceServiceImpl implements ConferenceService {
     @Override
     public void add(Conference entity) {
         conferenceRepository.save(entity);
+    }
+
+    @Override
+    public boolean signUpForConference(String name) {
+        Optional<Conference> optionalConference = conferenceRepository.findByName(name);
+        if (optionalConference.isPresent()){
+            Conference conference = optionalConference.get();
+            User user = userSecurity.getUserByEmailWithSpringSecurity();
+            Set<User> users = conference.getUsers();
+            users.add(user);
+            conferenceRepository.save(conference);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public List<ConferenceDto> getAllConferencesWhereThisUserIsRegistered() {
+        User user = userSecurity.getUserByEmailWithSpringSecurity();
+        List<Conference> conferences = conferenceRepository.findAll();
+        List<Conference> isRegisteredConferences = conferences.stream()
+                .filter(conference -> conference.getUsers().contains(user))
+                .collect(Collectors.toList());
+        return isRegisteredConferences.stream().map(ConferenceDto::fromConference).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ConferenceDto> getAllConferencesWhereThisUserIsNotRegistered() {
+        User user = userSecurity.getUserByEmailWithSpringSecurity();
+        List<Conference> conferences = conferenceRepository.findAll();
+        List<Conference> isNotRegisteredConferences = conferences.stream()
+                .filter(conference -> !conference.getUsers().contains(user))
+                .collect(Collectors.toList());
+        return isNotRegisteredConferences.stream().map(ConferenceDto::fromConference).collect(Collectors.toList());
     }
 }
