1111b1e1384e7c1f0b00e28fe12ca090a539e617
Bulat <programming.languages.only@gmail.com>
2021-03-20 20:09:02 +0300

    #63 Creating dto - ReportDto.java

---
+++ BulConferences/src/main/java/ru/itis/conferences/dto/ReportDto.java
@@ -0,0 +1,39 @@
+package ru.itis.conferences.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.itis.conferences.models.Report;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReportDto {
+
+    private String name;
+    private ConferenceDto conferenceDto;
+    private LocalDateTime startDate;
+    private LocalDateTime finishDate;
+
+    public Report toReport() {
+        return Report.builder()
+                .name(name)
+                .conference(conferenceDto.toConference())
+                .startDate(startDate)
+                .finishDate(finishDate)
+                .build();
+    }
+
+    public static ReportDto fromReport(Report report) {
+        return ReportDto.builder()
+                .name(report.getName())
+                .conferenceDto(ConferenceDto.fromConference(report.getConference()))
+                .startDate(report.getStartDate())
+                .finishDate(report.getFinishDate())
+                .build();
+    }
+}
