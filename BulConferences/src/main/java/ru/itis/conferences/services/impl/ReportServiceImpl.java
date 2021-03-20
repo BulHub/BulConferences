e98f0f9bc753b7ded40915a6beba761402271654
Bulat <programming.languages.only@gmail.com>
2021-03-20 13:47:44 +0300

    #35 Updating service - ReportServiceImpl.java

--- BulConferences/src/main/java/ru/itis/conferences/services/impl/ReportServiceImpl.java
+++ BulConferences/src/main/java/ru/itis/conferences/services/impl/ReportServiceImpl.java
@@ -2,10 +2,12 @@ package ru.itis.conferences.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 import ru.itis.conferences.models.Audience;
 import ru.itis.conferences.models.Report;
 import ru.itis.conferences.repositories.ReportRepository;
 import ru.itis.conferences.services.AudienceService;
+import ru.itis.conferences.services.ConferenceService;
 import ru.itis.conferences.services.ReportService;
 
 import java.time.LocalDateTime;
@@ -17,11 +19,13 @@ public class ReportServiceImpl implements ReportService {
 
     private final ReportRepository reportRepository;
     private final AudienceService audienceService;
+    private final ConferenceService conferenceService;
 
     @Autowired
-    public ReportServiceImpl(ReportRepository reportRepository, AudienceService audienceService) {
+    public ReportServiceImpl(ReportRepository reportRepository, AudienceService audienceService, ConferenceService conferenceService) {
         this.reportRepository = reportRepository;
         this.audienceService = audienceService;
+        this.conferenceService = conferenceService;
     }
 
     @Override
@@ -45,8 +49,30 @@ public class ReportServiceImpl implements ReportService {
     }
 
     @Override
-    public List<Report> findAllByAudience(Audience audience) {
-        return reportRepository.findAllByAudience(audience);
+    public List<Report> findByAudience(Audience audience) {
+        return reportRepository.findByAudience(audience);
+    }
+
+    @Override
+    public void fillingTheEntity(Report report, String conference,
+                                 String audience, String start_date,
+                                 String finish_date){
+        report.setConference(conferenceService.find(conference).get());
+        report.setAudience(audienceService.findByNumber(Long.parseLong(audience)).get());
+        report.setStartDate(LocalDateTime.parse(start_date));
+        report.setFinishDate(LocalDateTime.parse(finish_date));
+    }
+
+    @Override
+    public StringBuilder checkingDataForCreateReports(Report report, ModelMap map){
+        StringBuilder builder = new StringBuilder();
+        if (report.getStartDate() == null) builder.append("The start date of the report cannot be empty ");
+        if (report.getFinishDate() == null) builder.append("The end date of the report cannot be empty ");
+        Optional<Report> optionalReport = reportRepository.findByName(report.getName());
+        if (optionalReport.isPresent()){
+            builder.append("This conference name is already taken ");
+        }
+        return builder;
     }
 
     @Override
@@ -54,7 +80,13 @@ public class ReportServiceImpl implements ReportService {
         Optional<Audience> optionalAudience = audienceService.findByNumber(number);
         if(optionalAudience.isPresent()){
             Audience audience = optionalAudience.get();
-            List<Report> reports = findAllByAudience(audience);
+            List<Report> reports = findByAudience(audience);
+            for (Report report: reports) {
+                if(!(start.isAfter(report.getStartDate()) && finish.isAfter(report.getFinishDate()))
+                && !(start.isBefore(report.getStartDate()) && finish.isBefore(report.getFinishDate()))){
+                    return Optional.of(report);
+                }
+            }
         }
         return Optional.empty();
     }
