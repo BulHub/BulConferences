86fe670e76822253eaf3ed1717be5e9234c3cad3
Bulat <programming.languages.only@gmail.com>
2021-03-20 13:49:04 +0300

    #36 Updating repository - UserRepository.java

--- BulConferences/src/main/java/ru/itis/conferences/repositories/UserRepository.java
+++ BulConferences/src/main/java/ru/itis/conferences/repositories/UserRepository.java
@@ -2,12 +2,15 @@ package ru.itis.conferences.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.itis.conferences.models.Role;
 import ru.itis.conferences.models.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByToken(String token);
     Optional<User> findByEmail(String email);
+    List<User> findByRoles(Role role);
 }
