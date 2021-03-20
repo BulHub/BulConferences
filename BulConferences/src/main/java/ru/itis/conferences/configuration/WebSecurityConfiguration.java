a2ab4373536f2b45c722844c49554ceec4948b94
Bulat <programming.languages.only@gmail.com>
2021-03-20 17:56:52 +0300

    #48 Updating configuration - WebSecurityConfiguration.java

--- BulConferences/src/main/java/ru/itis/conferences/configuration/WebSecurityConfiguration.java
+++ BulConferences/src/main/java/ru/itis/conferences/configuration/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package ru.itis.conferences.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     /** Bean responsible for creating a class that hashes passwords */
