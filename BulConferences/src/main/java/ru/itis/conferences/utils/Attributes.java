18045d56603656cbec064ba28e493ab207401399
Bulat <programming.languages.only@gmail.com>
2021-03-20 17:55:52 +0300

    #45 Updating entity - Attributes.java

--- BulConferences/src/main/java/ru/itis/conferences/utils/Attributes.java
+++ BulConferences/src/main/java/ru/itis/conferences/utils/Attributes.java
@@ -1,6 +1,7 @@
 package ru.itis.conferences.utils;
 
 import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 public class Attributes {
 
@@ -14,5 +15,27 @@ public class Attributes {
         map.addAttribute("title", "Oops!");
         map.addAttribute("message", message);
         map.addAttribute("category", "error");
+    }
+
+    /**
+     * Method for adding a window where redirection occurs on successful process
+     * @param redirectAttributes Redirect model for adding a message
+     * @param message The message displayed in the window on the site
+     */
+    public static void addSuccessAttributesWithFlash(RedirectAttributes redirectAttributes, String message){
+        redirectAttributes.addFlashAttribute("title", "Good job!");
+        redirectAttributes.addFlashAttribute("message", message.isEmpty() ? "Success!" : message);
+        redirectAttributes.addFlashAttribute("category", "success");
+    }
+
+    /**
+     * Method for adding a window where redirection occurs in case of an unsuccessful process
+     * @param redirectAttributes Redirect model for adding a message
+     * @param message The message displayed in the window on the site
+     */
+    public static void addErrorAttributesWithFlash(RedirectAttributes redirectAttributes, String message){
+        redirectAttributes.addFlashAttribute("title", "Oops!");
+        redirectAttributes.addFlashAttribute("message", message.isEmpty() ? "Error!" : message);
+        redirectAttributes.addFlashAttribute("category", "error");
     }
 }
