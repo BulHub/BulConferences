74b30c505df9476cb28f81d1ff742d13ddac5465
Bulat <programming.languages.only@gmail.com>
2021-03-20 18:00:00 +0300

    #51 Updating application - ConferencesApplication.java

--- BulConferences/src/main/java/ru/itis/conferences/ConferencesApplication.java
+++ BulConferences/src/main/java/ru/itis/conferences/ConferencesApplication.java
@@ -3,17 +3,26 @@ package ru.itis.conferences;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import ru.itis.conferences.configuration.SenderConfig;
 
 import java.util.Properties;
 
+import static java.util.Collections.singletonList;
+
 @SpringBootApplication
+@Controller
 public class ConferencesApplication {
 
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    public ConferencesApplication(FreeMarkerConfigurer freeMarkerConfigurer) {
+        freeMarkerConfigurer.getTaglibFactory().setClasspathTlds(singletonList("/META-INF/security.tld"));
     }
 
     @Bean
