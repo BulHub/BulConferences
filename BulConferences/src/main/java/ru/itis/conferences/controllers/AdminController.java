101016ffc8d1933dc84b5c2c7a8006d5cc12dbcf
Bulat <programming.languages.only@gmail.com>
2021-03-20 17:54:46 +0300

    #44 Updating controller - AdminController.java

--- BulConferences/src/main/java/ru/itis/conferences/controllers/AdminController.java
+++ BulConferences/src/main/java/ru/itis/conferences/controllers/AdminController.java
@@ -3,13 +3,14 @@ package ru.itis.conferences.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.itis.conferences.dto.UserDto;
 import ru.itis.conferences.models.User;
 import ru.itis.conferences.services.RoleService;
 import ru.itis.conferences.services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +30,12 @@ public class AdminController {
     public String getAdminPage(ModelMap map){
         List<User> users = userService.findByRoles(roleService.findByName("ROLE_LISTENER").get());
         map.put("userDtos", users.stream().map(UserDto::fromUser).collect(Collectors.toList()));
+        return "users";
+    }
+
+    @PostMapping("/admin/users")
+    public String remoteUser(@RequestParam("email") String email){
+        //TODO: Как повысить роль?
         return "users";
     }
 }
